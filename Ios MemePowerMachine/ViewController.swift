ff144b7933098306d6ed5dfdf45bfeed4a447bf2
Luis Miguel Bravo <lmbgott@gmail.com>
2017-07-02 21:49:04 -0400

    hola

--- Ios MemePowerMachine/ViewController.swift
+++ Ios MemePowerMachine/ViewController.swift
@@ -7,18 +7,86 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    
 
+    @IBOutlet weak var maximoValorXLabel: UILabel!
+    @IBOutlet weak var xlabel: UILabel!
+    
+    
+    @IBOutlet weak var maximoValorYLabel: UILabel!
+    @IBOutlet weak var ylabel: UILabel!
+    
+    @IBOutlet weak var maximoValorZLabel: UILabel!
+    @IBOutlet weak var zlabel: UILabel!
+    
+    @IBOutlet weak var maximoValorMagnitudLabel: UILabel!
+    @IBOutlet weak var magnitudLabel: UILabel!
+
+    var maximoValorX:Double = 0
+    var maximoValorY:Double = 0
+    var maximoValorZ:Double = 0
+
+    
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        motionManager.accelerometerUpdateInterval = 0.001;
+        
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let myData = data {
+                // print(myData)
+                self.xlabel.text = String(myData.acceleration.x)
+                if (abs(myData.acceleration.x) > self.maximoValorX)
+                {
+                    self.maximoValorX = myData.acceleration.x
+                    self.maximoValorXLabel.text = String(self.maximoValorX)
+                }
+                
+                self.ylabel.text = String(myData.acceleration.y)
+                if (abs(myData.acceleration.y) > self.maximoValorY)
+                {
+                    self.maximoValorY = myData.acceleration.y
+                    self.maximoValorYLabel.text = String(self.maximoValorY)
+                }
+                
+                self.zlabel.text = String(myData.acceleration.z)
+                if (abs(myData.acceleration.z) > self.maximoValorZ)
+                {
+                    self.maximoValorZ = myData.acceleration.z
+                    self.maximoValorZLabel.text = String(self.maximoValorZ)
+                }
+                /*
+                self.sumaDeMagnitudes = abs(myData.acceleration.x) + abs(myData.acceleration.y) + abs(myData.acceleration.z)
+                self.maginitud.text = String(self.sumaDeMagnitudes)
+                if (self.sumaDeMagnitudes > self.maximoValorMagnitud) {
+                    self.maximoValorMagnitud = self.sumaDeMagnitudes
+                    self.maximoValorMagnitudLabel.text = String(self.maximoValorMagnitud)
+                }*/
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func ReiniciarContadores(_ sender: UIButton) {
+        self.maximoValorX = 0
+        self.maximoValorY = 0
+        self.maximoValorZ = 0
+        
+    }
+    
 
 
 }
